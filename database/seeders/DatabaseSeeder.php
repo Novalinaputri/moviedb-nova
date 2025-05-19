28cb7dbf84ef17a3e9bc5d4ce8e9cebf70f617f2
Novalinaputri <novalinaputri381@gmail.com>
2025-05-19 14:48:26 +0700

    seeding dan factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Movie; // Tambahkan baris ini
+use Database\Seeders\CategorySeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(CategorySeeder::class);
+        Movie::factory(50)->create();
     }
 }
